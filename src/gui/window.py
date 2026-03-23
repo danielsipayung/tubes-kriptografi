23cc71a79bb05fb151571c3c4001dadd5f772480
Daniel Sipayung <danielslearnstocode@gmail.com>
2026-03-24 06:40:41 +0700

    feat: add basic window

---
+++ src/gui/window.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("420x420")
+window.title("Steganografi LSB")
+
+icon = PhotoImage(file='assets\icon.png')
+window.iconphoto(True,icon)
+
+
+window.mainloop()

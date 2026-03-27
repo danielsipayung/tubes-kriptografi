7b476175c0825f633960cf1c3f3d86287b4d2e20
echaa0018 <18223082@std.stei.itb.ac.id>
2026-03-27 23:58:05 +0700

    add test_stega.py for testing

--- src/steganography/steganography.py
+++ src/steganography/steganography.py
@@ -225,63 +225,62 @@ def extract_secret(frames_folder, frame_order, mode="sequential", stego_key=None
     
     return header_dict, final_binary_bits
 
-# full test (embed and extract)
-while True:
-    test_folder_name = input("\nEnter frame folder name (in avi_frames/): ")
-    test_frames_path = os.path.join("avi_frames", test_folder_name)
-    frame_order = get_frame_order(test_frames_path, sensitivity=30.0)
-    # print(f"frame order: {frame_order}")
+# while True:
+#     test_folder_name = input("\nEnter frame folder name (in avi_frames/): ")
+#     test_frames_path = os.path.join("avi_frames", test_folder_name)
+#     frame_order = get_frame_order(test_frames_path, sensitivity=30.0)
+#     # print(f"frame order: {frame_order}")
 
-    action = input("embed/extract? (e/x): ").lower()
+#     action = input("embed/extract? (e/x): ").lower()
 
-    if action == 'e':
-        secret_type = input("Embed text or file? (t/f): ").lower()
+#     if action == 'e':
+#         secret_type = input("Embed text or file? (t/f): ").lower()
         
-        if secret_type == 't':
-            data_to_embed = input("Enter secret: ")
-            is_file = False
-        elif secret_type == 'f':
-            filename = input("Enter filename (in input/ folder): ")
-            data_to_embed = os.path.join("input", filename)
-            is_file = True
-        else:
-            print("Invalid choice.")
-            continue
+#         if secret_type == 't':
+#             data_to_embed = input("Enter secret: ")
+#             is_file = False
+#         elif secret_type == 'f':
+#             filename = input("Enter filename (in input/ folder): ")
+#             data_to_embed = os.path.join("input", filename)
+#             is_file = True
+#         else:
+#             print("Invalid choice.")
+#             continue
 
-        embed_mode = input("Mode (seq/ran): ").lower()
-        if embed_mode == "seq":
-            test_seq = generate_header(data_to_embed, is_file, is_random=False)
-            embed_secret(test_seq, test_frames_path, frame_order, mode="sequential")
-        elif embed_mode == "ran":
-            key = input("Enter stego-key: ")
-            test_rand = generate_header(data_to_embed, is_file, is_random=True)
-            embed_secret(test_rand, test_frames_path, frame_order, mode="random", stego_key=key)
+#         embed_mode = input("Mode (seq/ran): ").lower()
+#         if embed_mode == "seq":
+#             test_seq = generate_header(data_to_embed, is_file, is_random=False)
+#             embed_secret(test_seq, test_frames_path, frame_order, mode="sequential")
+#         elif embed_mode == "ran":
+#             key = input("Enter stego-key: ")
+#             test_rand = generate_header(data_to_embed, is_file, is_random=True)
+#             embed_secret(test_rand, test_frames_path, frame_order, mode="random", stego_key=key)
 
-    elif action == 'x':
-        extract_mode = input("Mode (seq/ran): ").lower()
+#     elif action == 'x':
+#         extract_mode = input("Mode (seq/ran): ").lower()
         
-        if extract_mode == "seq":
-            header, extracted_bits = extract_secret(test_frames_path, frame_order, mode="sequential")
-        elif extract_mode == "ran":
-            key = input("Enter stego-key: ")
-            header, extracted_bits = extract_secret(test_frames_path, frame_order, mode="random", stego_key=key)
-        else:
-            continue
+#         if extract_mode == "seq":
+#             header, extracted_bits = extract_secret(test_frames_path, frame_order, mode="sequential")
+#         elif extract_mode == "ran":
+#             key = input("Enter stego-key: ")
+#             header, extracted_bits = extract_secret(test_frames_path, frame_order, mode="random", stego_key=key)
+#         else:
+#             continue
 
-        if header:
-            if header["file_type"] == "text":
-                secret_text = binary_to_text(extracted_bits)
-                print(f"\nsecret message: {secret_text}")
+#         if header:
+#             if header["file_type"] == "text":
+#                 secret_text = binary_to_text(extracted_bits)
+#                 print(f"\nsecret message: {secret_text}")
                 
-            elif header["file_type"] == "file":
-                default_name = header["filename"]
-                print("\nsecret extracted")
-                print(f"file name: {default_name}")
-                save_name = input(f"save as (Enter to keep it as '{default_name}'): ")
+#             elif header["file_type"] == "file":
+#                 default_name = header["filename"]
+#                 print("\nsecret extracted")
+#                 print(f"file name: {default_name}")
+#                 save_name = input(f"save as (Enter to keep it as '{default_name}'): ")
                 
-                if save_name.strip() == "":
-                    save_name = default_name
+#                 if save_name.strip() == "":
+#                     save_name = default_name
                     
-                output_path = os.path.join("output", save_name)
-                binary_to_file(extracted_bits, output_path)
-                print(f"file saved to {output_path}")
+#                 output_path = os.path.join("output", save_name)
+#                 binary_to_file(extracted_bits, output_path)
+#                 print(f"file saved to {output_path}")
